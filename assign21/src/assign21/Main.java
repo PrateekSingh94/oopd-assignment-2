3749b944d5ca4b55b6f4f7e074c2861f97c94f1d
Prateek Singh <prateek15124@iiitd.ac.in>
2016-05-06 10:29:29 +0530

    Added

---
+++ assign21/src/assign21/Main.java
@@ -0,0 +1,12 @@
+package assign21;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fibonacii fb = new Fibonacii();
+		fb.fibonacci();
+
+	}
+
+}
